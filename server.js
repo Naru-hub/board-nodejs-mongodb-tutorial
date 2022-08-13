c87ab238012f443a18eec0caccc61cf4be1764c2
Naru-hub <1208ny0324@gmail.com>
2022-08-13 22:08:39 +0900

    全てのデータを取ってくるgetメソッドを追加

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 3000;
+const Thread = require("./models/Thread");
 require("dotenv").config();
 
 app.use(express.static("public"));
@@ -10,5 +11,15 @@ mongoose
   .connect(process.env.MONGOURL)
   .then(() => console.log("db connected"))
   .catch((err) => console.log(err));
+
+// getメソッド
+app.get("/api/v1/threads", async (req, res) => {
+  try {
+    const allThreads = await Thread.find({});
+    res.status(200).json(allThreads);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(PORT, console.log("server running"));
